60b3af508bc879d115b83d0cdbf8a9f557883d66
Daniel Andrus <danieleandrus@gmail.com>
2012-07-23 23:45:27 -0600

    Refactored the PreyFinder 9001 code

---
+++ src/preyfinder/IPreyFinderManager.java
@@ -0,0 +1,17 @@
+package preyfinder;
+
+import java.util.List;
+
+import org.bukkit.entity.Player;
+
+public interface IPreyFinderManager {
+
+	public void registerNewFinder(Player p);
+
+	public List<PreyFinder> getExpiredFinders();
+
+	public boolean removeFinder(PreyFinder f);
+
+	public boolean removeFinder(String s);
+
+}
