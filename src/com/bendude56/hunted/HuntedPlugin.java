00a78a0c209e4bc3ef6ede57215f6929cdff422e
Deaboy <danieleandrus@gmail.com>
2012-07-28 00:45:31 -0600

    Refactored everything, simplified the main class and stripped away unnecessary code.

--- src/com/bendude56/hunted/HuntedPlugin.java
+++ src/com/bendude56/hunted/HuntedPlugin.java
@@ -5,85 +5,76 @@ import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import preyfinder.IPreyFinderManager;
-import preyfinder.PreyFinderManager;
 
-import teams.ITeamManager;
-import teams.TeamManager;
 
-import com.bendude56.hunted.config.SettingsManager;
-import com.bendude56.hunted.loadout.LoadoutManager;
+import com.bendude56.hunted.finder.IFinderManager;
+import com.bendude56.hunted.finder.FinderManager;
+import com.bendude56.hunted.loadouts.LoadoutManager;
+import com.bendude56.hunted.settings.SettingsManager;
+import com.bendude56.hunted.teams.ITeamManager;
+import com.bendude56.hunted.teams.TeamManager;
 
 public class HuntedPlugin extends JavaPlugin {
 
 	private Logger log = Logger.getLogger("Minecraft");
-	public boolean spoutEnabled;
-	public SpConnect spoutConnect;
-	private World manhuntWorld;
-	private SettingsManager settings;
-	private LoadoutManager loadouts;
-	private ITeamManager teams;
-	private IPreyFinderManager finders;
-	private Game game;
+	
+	private SettingsManager	settings;
+	private LoadoutManager	loadouts;
+	private ITeamManager	teams;
+	private IFinderManager	finders;
+	private ManhuntGame		game;
+	public SpConnect		spoutConnect;
+	
+	public boolean	spoutEnabled;
+	private World 	manhuntWorld;
 
 	@Override
-	public void onDisable() {
-		for (String s : getGame().getCreative()) {
-			if (Bukkit.getPlayerExact(s) != null) {
-				Bukkit.getPlayerExact(s).setGameMode(GameMode.CREATIVE);
-			}
-		}
-		for (Player p : Bukkit.getOnlinePlayers()) {
-			p.sendMessage(ChatColor.DARK_RED + getDescription().getName()
-					+ ChatColor.RED + " plugin has been disabled!");
-		}
-		log(Level.INFO, "Unloaded from memory...");
-	}
-
-	@Override
-	public void onEnable() {
+	public void onEnable()
+	{
 		
-		if (getServer().getPluginManager().isPluginEnabled("Spout")) {
+		//Start up all the classes.
+		if (getServer().getPluginManager().isPluginEnabled("Spout"))
+		{
 			spoutEnabled = true;
 			spoutConnect = new SpConnect();
-		} else {
-			log(Level.WARNING, "Spout installation not detected!");
+		}
+		else
+		{
 			spoutEnabled = false;
 		}
 
-		manhuntWorld = Bukkit.getWorlds().get(0);
-		settings = new SettingsManager();
-		if (settings.WORLD.value != manhuntWorld.getName()
-				&& Bukkit.getWorld(settings.WORLD.value) != null) {
-			manhuntWorld = Bukkit.getWorld(settings.WORLD.value);
-		}
-		loadouts = new LoadoutManager();
-		teams = new TeamManager();
-		finders = new PreyFinderManager();
-		
-		game = new Game();
+		settings =	new SettingsManager();
+		loadouts =	new LoadoutManager();
+		teams =		new TeamManager();
+		finders =	new FinderManager();
+		game =		new ManhuntGame();
 		new CmdExec();
+		
+		manhuntWorld = Bukkit.getWorld(settings.WORLD.value);
+		
 		//Register Events
 		getServer().getPluginManager().registerEvents(new HuntedPlayerListener(), this);
 		getServer().getPluginManager().registerEvents(new HuntedBlockListener(), this);
 		getServer().getPluginManager().registerEvents(new HuntedEntityListener(), this);
 		
-		// new HuntedInventoryListener();
-		log(Level.INFO, "Version " + getDescription().getVersion()
-				+ " loaded into memory...");
+	}
+
+	@Override
+	public void onDisable()
+	{
+		
+		//Save players who are in creative mode in the Team Manager
+		
 		for (Player p : Bukkit.getOnlinePlayers()) {
-			if (p.getWorld() == getWorld()) {
-				game.onLogin(p);
-			}
 			p.sendMessage(ChatColor.DARK_RED + getDescription().getName()
-					+ " v" + getDescription().getVersion() + ChatColor.YELLOW
-					+ " is up running!");
+					+ ChatColor.RED + " has been disabled.");
 		}
+		
+		log(Level.INFO, "Unloaded from memory.");
 	}
 
 	public void log(Level level, String message) {
@@ -91,8 +82,7 @@ public class HuntedPlugin extends JavaPlugin {
 	}
 
 	public static HuntedPlugin getInstance() {
-		return (HuntedPlugin) Bukkit.getServer().getPluginManager()
-				.getPlugin("Manhunt");
+		return (HuntedPlugin) Bukkit.getServer().getPluginManager().getPlugin("Manhunt");
 	}
 
 	public World getWorld() {
@@ -112,12 +102,13 @@ public class HuntedPlugin extends JavaPlugin {
 		return teams;
 	}
 	
-	public IPreyFinderManager getPreyFinder()
+	public IFinderManager getFinders()
 	{
 		return finders;
 	}
 
-	public Game getGame() {
+	public ManhuntGame getGame() {
 		return game;
 	}
+
 }
