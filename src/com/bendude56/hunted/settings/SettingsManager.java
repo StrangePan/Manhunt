89f140910e35be4069bd549ae74becc66ae596d3
Deaboy <danieleandrus@gmail.com>
2012-07-30 23:18:58 -0600

    Reordered a few settings

--- src/com/bendude56/hunted/settings/SettingsManager.java
+++ src/com/bendude56/hunted/settings/SettingsManager.java
@@ -34,7 +34,6 @@ public class SettingsManager
 	public final Setting<Boolean> INSTANT_DEATH;
 	public final Setting<Boolean> FLYING_SPECTATORS;
 	public final Setting<Boolean> LOADOUTS;
-	public final Setting<Boolean> NORTH_COMPASS;
 	public final Setting<Boolean> TEAM_HATS;
 	public final Setting<Boolean> NO_BUILD;
 	
@@ -43,10 +42,11 @@ public class SettingsManager
 	public final Setting<Boolean> ENVIRONMENT_RESPAWN;
 	public final Setting<Boolean> ENVIRONMENT_DEATH;
 	public final Setting<Boolean> PREY_FINDER;
+	public final Setting<Boolean> NORTH_COMPASS;
+	public final Setting<Integer> FINDER_COOLDOWN;
 	
 	public final Setting<Integer> OFFLINE_TIMEOUT;
 	public final Setting<Integer> DAY_LIMIT;
-	public final Setting<Integer> FINDER_COOLDOWN;
 	public final Setting<Integer> SETUP_TIME;
 	
 	public final Setting<Integer> BOUNDARY_WORLD;
@@ -79,7 +79,6 @@ public class SettingsManager
 		settings.add(INSTANT_DEATH = new Setting<Boolean>("instantDeath", false, FILE_MAIN, "Players will die in one hit.", "Players take damage like normal."));
 		settings.add(FLYING_SPECTATORS = new Setting<Boolean>("flyingSpectators", true, FILE_MAIN, "Spectators can fly in creative mode.", "Spectators are bound to the ground."));
 		settings.add(LOADOUTS = new Setting<Boolean>("loadouts", true, FILE_MAIN, "Players will recieve predefined loadouts.", "Players will start with empty inventories."));
-		settings.add(NORTH_COMPASS = new Setting<Boolean>("northCompass", true, FILE_MAIN, "Compasses will always point north.", "Compasses will always point towards spawn."));
 		settings.add(TEAM_HATS = new Setting<Boolean>("teamHats", true, FILE_MAIN, "Players get special, identifying hats.", "Players do not have special hats."));
 		settings.add(NO_BUILD = new Setting<Boolean>("noBuild", true, FILE_MAIN, "Players cannot build unless the game is running.", "Players can edit the world when the game is not running."));
 		
@@ -87,14 +86,15 @@ public class SettingsManager
 		settings.add(HOSTILE_MOBS = new Setting<Boolean>("hostileMobs", true, FILE_MAIN, "Hostile mobs are enabled.", "Hostile mobs are disabled."));
 		settings.add(ENVIRONMENT_RESPAWN = new Setting<Boolean>("envRespawn", true, FILE_MAIN, "Players will respawn when dying from the environment.", "Players will not respawn when dying from the environment."));
 		settings.add(ENVIRONMENT_DEATH = new Setting<Boolean>("envDeath", false, FILE_MAIN, "Players can die from the environment.", "Players cannot die from the environment."));
-		settings.add(PREY_FINDER = new Setting<Boolean>("preyFinder", true, FILE_MAIN, "Hunters may use the Prey Finder.", "The compass is just a regular compass."));
 		
 		settings.add(OFFLINE_TIMEOUT = new Setting<Integer>("offlineTimeout", 60, FILE_MAIN, "How many seconds until offline players are disqualified.", "Offline players will not be disqualified."));
 		settings.add(DAY_LIMIT = new Setting<Integer>("dayLimit", 3, FILE_MAIN, "How many dats the game will last.", "The manhunt game will never end."));
-		settings.add(FINDER_COOLDOWN = new Setting<Integer>("finderCooldown", 3, FILE_MAIN, "The Preyfinder cooldown delay in minutes.", "The PreyFinder has no cooldown delay."));
 		settings.add(SETUP_TIME = new Setting<Integer>("setupTime", 10, FILE_MAIN, "How many minutes the prey have to prepare.", "The game starts immediately with no setup."));
-
-
+		
+		settings.add(PREY_FINDER = new Setting<Boolean>("preyFinder", true, FILE_MAIN, "Hunters may use the Prey Finder.", "The compass is just a regular compass."));
+		settings.add(FINDER_COOLDOWN = new Setting<Integer>("finderCooldown", 180, FILE_MAIN, "The Preyfinder cooldown delay in seconds.", "The PreyFinder has no cooldown delay."));
+		settings.add(NORTH_COMPASS = new Setting<Boolean>("northCompass", true, FILE_MAIN, "Compasses will always point north.", "Compasses will always point towards spawn."));
+		
 		this.world_path = (Bukkit.getWorld(WORLD.value) == null ? Bukkit.getWorlds().get(0).getName() : WORLD.value) + "/Manhunt";
 		files.add(FILE_WORLD = new SettingsFile("World Config", world_path, "Config"));
 		
