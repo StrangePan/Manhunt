449496510c4ad0e69fed3a8841113442fc91b92b
Deaboy <danieleandrus@gmail.com>
2012-07-30 23:57:03 -0600

    Added new GameUtil class

---
+++ src/com/bendude56/hunted/games/GameUtil.java
@@ -0,0 +1,31 @@
+package com.bendude56.hunted.games;
+
+import com.bendude56.hunted.teams.TeamManager.Team;
+
+/**
+ * Contains various static methods for messages and other game-related things.
+ * @author Deaboy
+ *
+ */
+public class GameUtil {
+
+	/**
+	 * Broadcasts to all palyers the results of the match.
+	 * @param winners
+	 * @param losers
+	 */
+	protected static void broadcastManhuntWinners(Team winners, Team losers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Broadcasts to all palyers that a player has forfeit.
+	 * @param player_name
+	 */
+	protected static void broadcastPlayerForfeit(String player_name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
