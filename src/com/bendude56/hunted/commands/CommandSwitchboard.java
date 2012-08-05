5715c5930bbb3f731720799a6df608e857cecc27
Deaboy <danieleandrus@gmail.com>
2012-08-04 18:32:56 -0600

    Finished setting up the command switchboard

--- src/com/bendude56/hunted/commands/CommandSwitchboard.java
+++ src/com/bendude56/hunted/commands/CommandSwitchboard.java
@@ -63,16 +63,81 @@ public class CommandSwitchboard implements CommandExecutor
 		
 		//LOADOUT COMMANDS
 		
-		if (arg.equalsIgnoreCase("loadout") || arg.equalsIgnoreCase("inv")) //TODO FINISH
+		if (arg.equalsIgnoreCase("loadout") || arg.equalsIgnoreCase("loadouts") || arg.equalsIgnoreCase("inv") || arg.equalsIgnoreCase("invs"))
 		{
-			//
+			//List loadouts
+		}
+		if (arg.equalsIgnoreCase("saveloadout") || arg.equalsIgnoreCase("saveinv") || arg.equalsIgnoreCase("newloadout") || arg.equalsIgnoreCase("newinv")) //TODO FINISH
+		{
+			//Save a new loadout
+		}
+		if (arg.equalsIgnoreCase("loadloadout") || arg.equalsIgnoreCase("loadinv"))
+		{
+			//Load a loadout
+		}
+		if (arg.equalsIgnoreCase("deleteloadout") || arg.equalsIgnoreCase("delloadout") || arg.equalsIgnoreCase("delinv") || arg.equalsIgnoreCase("deleteinv"))
+		{
+			//Delete a loadout
+		}
+		if (arg.equalsIgnoreCase("hunterloadout") || arg.equalsIgnoreCase("hunterinv"))
+		{
+			//Sets the hunter loadout or loads it
+		}
+		if (arg.equalsIgnoreCase("preyloadout") || arg.equalsIgnoreCase("preyinv"))
+		{
+			//Sets the prey loadout or loads it
 		}
 		
 		//COMMANDS FOR SETTING UP THE TEAMS
 		
 		if (arg.equalsIgnoreCase("hunter"))
 		{
-			
+			//Changes a player to team hunter
+		}
+		if (arg.equalsIgnoreCase("prey"))
+		{
+			//Changes a player to team prey
+		}
+		if (arg.equalsIgnoreCase("spectator"))
+		{
+			//Changes a player to team spectator
+		}
+		
+		//COMMANDS FOR CONTORLLING PLAYERS
+		
+		if (arg.equalsIgnoreCase("lock"))
+		{
+			//Locks teams
+		}
+		if (arg.equalsIgnoreCase("kick"))
+		{
+			//Kicks a player from the manhunt game
+		}
+		if (arg.equalsIgnoreCase("quit"))
+		{
+			//Quits the manhunt game
+		}
+		
+		//COMMANDS FOR EDITING SPAWN
+		
+		if (arg.equalsIgnoreCase("spawn"))
+		{
+			//Teleports the player to a spawn
+		}
+		if (arg.equalsIgnoreCase("setspawn"))
+		{
+			//Sets a spawn point
+		}
+		
+		//COMMANDS FOR STARTING/STOPING THE GAME
+		
+		if (arg.equalsIgnoreCase("startgame"))
+		{
+			//Starts the manhunt game
+		}
+		if (arg.equalsIgnoreCase("stopgame"))
+		{
+			//Stops the game
 		}
 		
 		
